d32e536dbb77356dd53f9895b56fae92b00cdac8
OlgaV0539 <olv-personal@mail.ru>
2025-01-22 16:59:30 +0700

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -16,14 +16,13 @@ async def user_admin():
     return {"message": "Вы вошли как администратор"}
 
 
-# Создание маршрута c параметром
 @app.get("/user/{user_id}")
 async def read_user(user_id: int):
-    return {"user_id": user_id, "name": f"Вы вошли как пользователь № {user_id}"}
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 
-# Создание маршрута c двумя параметрами
 @app.get("/users/{user_name}/ages/{age_id}")
 async def read_user_age(user_name: str, age_id: int):
-    return {"user_name": user_name, "age_id": age_id, "name":
-            f"Информация о пользователе. Имя:{user_name}, Возраст: {age_id}"}
+    return {
+        "message": f"Информация о пользователе. Имя: {user_name}, Возраст: {age_id}"
+    }
