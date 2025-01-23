ed4e4c67fdeb0f9f6da208a1ffcc25db388d7118
OlgaV0539 <olv-personal@mail.ru>
2025-01-23 13:49:07 +0700

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -10,22 +10,20 @@ users: Dict[str, Dict[str, str]] = {}
 # Определение базового маршрута
 @app.get("/")
 async def root():
-    return {"message": "Главная страница"}
+    return "Главная страница"
 
 
 # Создание маршрута к странице администратора
 @app.get("/user/admin")
 async def user_admin():
-    return {"message": "Вы вошли как администратор"}
+    return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
 async def read_user(user_id: int):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
-
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.get("/user")
 async def read_user_age(user_name: str, age_id: int):
-    return {
-        "message": f"Информация о пользователе. Имя: {user_name}, Возраст: {age_id}"
-    }
+    return f"Информация о пользователе. Имя: {user_name}, Возраст: {age_id}"
+    
